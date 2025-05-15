501432b5d60736bd5583476bfbcef9e052890936
JericoFX <blackjordison@gmail.com>
2025-05-15 11:48:37 -0300

    nuevo

--- client/init.lua
+++ client/init.lua
@@ -1,111 +1,64 @@
-local Bridge = exports.community_bridge.Bridge()
-
-local function setEventName(name)
-	return string.format("communityVehicleTransfer::%s::%s", not IsDuplicityVersion() and "server" or "client", name)
-end
+local QBCore = exports["qb-core"]:GetCoreObject()
 
 local function getClosestVehicle()
-	local vehicle, vehicleCoords = lib.getClosestVehicle(GetEntityCoords(cache.ped), 5.0, true)
+	local vehicle = lib.getClosestVehicle(GetEntityCoords(cache.ped), 4.0, true)
 	return vehicle
 end
-local function getVehiclePlate()
+
+local function getVehiclePlate(vehicle)
+	local plate = GetVehicleNumberPlateText(vehicle)
+	return plate
+end
+
+local function openUI(bool, data)
+	if bool then
+		SetNuiFocus(true, true)
+		SendNUIMessage({
+			action = "openUI",
+			data = data
+		})
+	else
+		SetNuiFocus(false, false)
+		SendNUIMessage({
+			action = "closeUI"
+		})
+	end
+end
+
+AddEventHandler("communityVehicle:client:openUI", function()
 	local vehicle = getClosestVehicle()
 	if vehicle then
-		return GetVehicleNumberPlateText(vehicle)
-	end
-	return nil
-end
-
-local function getVehicleOwner()
-	local vehicle = getClosestVehicle()
-	if not vehicle then
-		lib.lib.notify({
-			title = 'Vehicle Transfer',
-			description = 'No vehicle found nearby.',
-			type = 'error'
-		})
-		return nil
-	end
-	local isOwner = lib.callback.await(setEventName("getVehicleOwner"), false, getVehiclePlate())
-	return isOwner
-end
-
-local function getClosestPlayer()
-	local player, playerCoords = lib.getClosestPlayer(GetEntityCoords(cache.ped), 5.0, true)
-	if player then
-		return { player = player, GetPlayerServerId = GetPlayerServerId(player) }
-	end
-	return nil
-end
-
---- This will open the UI if is true
----@param bool boolean open or close the UI
----@param data {currentOwner:string,newOwner:string, signed:boolean,vehiclePlate:string}
-local function openUI(bool, data)
-	SetNuiFocus(bool, bool)
-	SendNUIMessage({
-		action = bool and "openUI" or "closeUI",
-		data = bool and data or nil
-	})
-end
-
-local function setCurrentOwnerContractAsSigned(data, cb)
-	local currentOwner, newOwner, signed, vehiclePlate = data.currentOwner, data.newOwner, data.signed, data
-		.vehiclePlate
-	Bridge.Callback.Trigger(setEventName("setContractAsSigned"), currentOwner, newOwner, signed, vehiclePlate)
-	cb("ok")
-end
-
-
-
-local function setNewOwnerContractAsSigned(data, cb)
-	local signed = data.currentOwnerSigned and not data.newOwnerSigned
-
-	local isSuccess = lib.callback.await("communityVehicleTransfer::server::setNewOwnerContractAsSigned", false, data)
-	if isSuccess then
+		local plate = getVehiclePlate(vehicle)
+		local getOwner = lib.callback.await("communityVehicle:server:getOwner", 1000, plate)
+		if not getOwner then
+			lib.notify({
+				title = "No Vehicle Found",
+				description = "You are not the owner of this vehicle.",
+				type = "error"
+			})
+			return
+		end
+		openUI(true, getOwner)
+	else
 		lib.notify({
-			title = 'Vehicle Transfer',
-			description = 'You have successfully signed the contract',
-			type = 'success'
-		})
-	end
-	cb("ok")
-end
-
-local function checkVehicleOwner(data, cb)
-	local plate = data.plate
-	local isValid = Bridge.Callback.Trigger(setEventName("checkVehicleOwner"), plate)
-	cb(isValid)
-end
-
-RegisterNetEvent("communityVehicleTransfer::client::setClientContractData", function(data)
-	openUI(true, data)
-end)
-
-AddEventHandler("communityVehicleTransfer::client::openUI", function(data)
-	local information = lib.callback.await("communityVehicleTransfer::server::getData", false, getClosestPlayer(),
-		getVehiclePlate())
-	if not information then
-		lib.notify({
-			title = 'Vehicle Transfer',
-			description = 'No vehicle found nearby.',
-			type = 'error'
+			title = "No Vehicle Found",
+			description = "You are not near a vehicle.",
+			type = "error"
 		})
 		return
 	end
-	openUI(true, information)
 end)
 
-AddEventHandler("communityVehicleTransfer::client::closeUI", function()
-	SetNuiFocus(false, false)
+AddEventHandler("communityVehicle:client:signNewOwner", function(data)
+	openUI(true, data)
 end)
+
 RegisterNUICallback("closeUI", function(data, cb)
-	SetNuiFocus(false, false)
+	openUI(false)
 	cb("ok")
 end)
 
-RegisterNUICallback("setCurrentOwnerContractAsSigned", setCurrentOwnerContractAsSigned)
-
-RegisterNUICallback("setNewOwnerContractAsSigned", setNewOwnerContractAsSigned)
-
-RegisterNUICallback("checkVehicleOwner", checkVehicleOwner)
+RegisterNUICallback("signNewOwner", function(data, cb)
+	local result = lib.callback("communityVehicle:server:signNewOwner", false, data)
+	cb("ok")
+end)
--- server/init.lua
+++ server/init.lua
@@ -1,48 +1,41 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
-local currentContracts = {}
+local activeTransfers = {}
 
-local function getVehicleMetadata(owner, plate)
-	local Player = QBCore.Functions.GetPlayer(owner)
-	if not Player then return end
-	local vehicle = Mysql.single.await(
-		"SELECT vehicle, JSON_EXTRACT(mods,'$.color1') as color1, JSON_EXTRACT(mods,'$.color2') as color2 FROM player_vehicles WHERE citizenid = ? AND plate = ?",
-		{ Player.PlayerData.citizenid, plate })
-	if vehicle then
-		local color1 = vehicle.color1 and json.decode(vehicle.color1) or nil
-		local color2 = vehicle.color2 and json.decode(vehicle.color2) or nil
-		local veh = vehicle.vehicle
-		return {
-			color1 = color1,
-			color2 = color2,
-			vehicle = veh
-		}
-	end
-end
-
-lib.callback.register("communityVehicleTransfer::server::getCurrentContracts", function()
-	return currentContracts
+lib.callback.register("communityVehicle:server:getOwner", function(source, plate)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if not Player then
+        return false
+    end
+    local vehicle = {}
+    local result = MySQL.single.await("SELECT * FROM player_vehicles WHERE plate = ? AND owner = ?",
+        { plate, Player.PlayerData.citizenid })
+    if result.owner then
+        local mods = json.decode(result.mods)
+        vehicle = {
+            plate = result.plate,
+            model = result.vehicle,
+            mileage = result.mileage
+        }
+        activeTransfers[Player.PlayerData.source] = {
+            plate = result.plate,
+            model = result.vehicle,
+            mileage = result.mileage,
+            newOwner = false,
+            newOwnerSign = false,
+            currentOwner = Player.PlayerData.citizenid,
+            currentOwnerSign = false
+        }
+    end
+    return result and vehicle or false
 end)
 
-lib.callback.register("communityVehicleTransfer::server::setNewOwnerContractAsSigned", function(source, data)
-	if data.signed then
-		local Player = QBCore.Functions.GetPlayer(source)
-		local newOwner = QBCore.Functions.GetPlayerByCitizenId(data.newOwner)
-		if not Player or not newOwner then
-			return false
-		end
-		currentContracts[data.newOwner] = {
-			currentOwner = data.currentOwner,
-			ownerServerId = source,
-			newOwner = data.newOwner,
-			newOwnerServerId = newOwner.PlayerData.source,
-			currentOwnerSigned = data.currentOwnerSigned,
-			newOwnerSigned = false,
-			vehiclePlate = data.vehiclePlate,
-			mods = {}
-		}
-		TriggerClientEvent("communityVehicleTransfer::client::setClientContractData", newOwner.PlayerData.source,
-			currentContracts[data.newOwner])
-		return true
-	end
-	return false
+--https://overextended.dev/ox_lib/Modules/AddCommand/Server
+lib.addCommand("cTransferVehicle", {
+    help = "Transfer a vehicle to another player",
+    params = {
+
+    },
+    restricted = 'group.user'
+}, function(source, args, raw)
+    TriggerClientEvent("communityVehicle:client:openUI", source)
 end)
